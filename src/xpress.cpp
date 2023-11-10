a980734f7589bea0908c9a367d52dc443919125e
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-11-10 13:55:51 -0500

    plain lz77 will be implemented first as it is simpler and is a necessary step towards lz77+huffman

--- src/xpress.cpp
+++ src/xpress.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 #include <cstddef>
 #include <bitset>
-
-std::byte b{42};
-
-std::ostream& operator<<(std::ostream& os, std::byte b) {
-    return os << std::bitset<8>(std::to_integer<int>(b));
-}
+#include <cstdlib>
 
 int main() {
-  std::cout << b << std::endl;
+std::cout << "test" << std::endl;
 
-  return 0;
+  return EXIT_SUCCESS;
 }
